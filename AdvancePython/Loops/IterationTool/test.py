d019c27da52eaed76e039c764cb45209fc4d3fa4
showrav dhar <dhardip349900@gmail.com>
2024-10-04 21:39:52 +0600

    updated

---
+++ AdvancePython/Loops/IterationTool/test.py
@@ -0,0 +1,10 @@
+
+ls = [1,2,3,4,5]
+i = iter(ls) # holds the address of the list's first item
+print(i)
+print(i.__next__())
+print(i.__next__())
+print(i.__next__())
+print(i.__next__())
+print(i.__next__())
+# done
