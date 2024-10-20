da5b1d02ed2f74cb461b9057c18263231d26f107
showrav dhar <dhardip349900@gmail.com>
2024-10-20 13:30:32 +0600

    oop

--- AdvancePython/ObjectOrientedProgramming/01_solve.py
+++ AdvancePython/ObjectOrientedProgramming/01_solve.py
@@ -60,6 +60,94 @@
 # d1 = Animal("Tommy", 20)
 # print(d1.__Name)
 
+# class computer:
+#     def __init__(self):
+#         self.__maxprice = 1000
+#
+#     def sellingPrice(self):
+#         print(f"Selling price = {self.__maxprice}")
+#
+#     def priceSetter(self,price):
+#         self.__maxprice = price
+#
+#
+# c = computer()
+# c.priceSetter(1500)
+# c.sellingPrice()
+
 
 # 5. Polymorphism Problem: Demonstrate polymorphism by defining a method fuel_type in both Car and ElectricCar
 # classes, but with different behaviors.
+
+# class Car:
+#
+#     # have to pass [self] keyword every time you create a function inside a class
+#     def __init__(self, brand, model):  # constructor
+#         self.brand = brand
+#         self.model = model
+#
+#     def details(self):
+#         print(f"Brand Name = {self.brand} , Car Model - {self.model}")
+#
+#     def fuel_type(self):
+#         print(f"Fule type = Disel")
+#
+#
+# class ElectricCar(Car):
+#     def __init__(self, brand, model, batterySize):
+#         super().__init__(brand, model)
+#         self.batterySize = batterySize
+#
+#     def fuel_type(self):
+#         print(f"Fule type = Electric Charge")
+#
+#
+# car1 = Car("Toyota", "Corolla")
+#
+# ev1 = ElectricCar("Tesla", "Model X", 100000)
+#
+# ev1.fuel_type()
+# car1.fuel_type()
+
+# 6. Class Variables
+# Problem: Add a class variable to Car that keeps track of the number of cars created.
+
+# class Car:
+#     total_cars = 0
+#
+#     # have to pass [self] keyword every time you create a function inside a class
+#     def __init__(self, brand, model):  # constructor
+#         self.brand = brand
+#         self.model = model
+#         self.total_cars += 1
+#
+#     def details(self):
+#         print(f"Brand Name = {self.brand} , Car Model - {self.model}")
+#
+#     def fuel_type(self):
+#         print(f"Fule type = Disel")
+#
+#
+# car1 = Car("Tata", "Nexon")
+# car2 = Car("Tata", "Alto")
+# car3 = Car("Tata", "OMNI")
+# print(Car.total_cars)
+
+
+# 7. Static Method
+# Problem: Add a static method to the Car class that returns a general description of a car.
+
+class Bike:
+
+    def __init__(self, Brand, Name):
+        self.Brand = Brand
+        self.Name = Name
+
+    @staticmethod
+    def general_description():
+        return "Bike is Two Wheeler"
+
+
+b1 = Bike("Pulser", "Bajaj")
+print(b1.general_description())
+print(Bike.general_description())
